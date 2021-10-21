9e6f05f06fcc86c8c8cdd1e2c903d849bcba6592
caitlin.sarro <caitlin.sarro@gmail.com>
2021-10-21 13:39:39 +0200

    Delete Packages and Formulas.R

--- libaries and formulas/Packages and Formulas.R
+++
@@ -1,8 +0,0 @@
-
-# Here to progress and sources of installing the relevant packages is saved 
-
-#This is supposed to load all relevent packages 
-# source https://geocompr.robinlovelace.net/spatial-class.html
-remotes::install_github("geocompr/geocompkg") 1
-
-install.packages("sf")
