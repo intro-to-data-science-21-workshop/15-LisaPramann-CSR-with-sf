0c24f172557f0223eae9944463c5037be4e89467
caitlin.sarro <caitlin.sarro@gmail.com>
2021-10-21 13:36:46 +0200

    cleaned up

---
+++ libaries_and_formulas/Packages and Formulas.R
@@ -0,0 +1,8 @@
+
+# Here to progress and sources of installing the relevant packages is saved 
+
+#This is supposed to load all relevent packages 
+# source https://geocompr.robinlovelace.net/spatial-class.html
+remotes::install_github("geocompr/geocompkg") 1
+
+install.packages("sf")
